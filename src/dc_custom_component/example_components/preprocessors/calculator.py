f68968375dc03d59cc5afd7dd474513992ceca4f
TheMightyJonez <71824233+TheMightyJonez@users.noreply.github.com>
2025-01-14 04:48:31 -0500

    v7
    component

--- src/dc_custom_component/example_components/preprocessors/calculator.py
+++ src/dc_custom_component/example_components/preprocessors/calculator.py
@@ -1,10 +1,10 @@
 from haystack import component
-
-class Calculator(component):
+@component
+class Calculator:
 
     def run(self, query: str, **kwargs):
         try:
             result = eval(query)
         except Exception as e:
             result = None
-        return {"result": result}
+        return {"answers": result}
