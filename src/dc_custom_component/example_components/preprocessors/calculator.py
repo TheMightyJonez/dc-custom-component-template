1f10509c0240c293b421d74c681888c6a5d5e720
TheMightyJonez <71824233+TheMightyJonez@users.noreply.github.com>
2025-01-14 04:40:59 -0500

    reduces
    v5

--- src/dc_custom_component/example_components/preprocessors/calculator.py
+++ src/dc_custom_component/example_components/preprocessors/calculator.py
@@ -1,8 +1,6 @@
 from haystack.nodes import Component
 
 class Calculator(Component):
-    def __init__(self):
-        super().__init__()
 
     def run(self, query: str, **kwargs):
         try:
@@ -10,6 +8,3 @@ class Calculator(Component):
         except Exception as e:
             result = None
         return {"result": result}, "output_1"
-
-    def to_dict(self):
-        return {"type": self.__class__.__name__}
